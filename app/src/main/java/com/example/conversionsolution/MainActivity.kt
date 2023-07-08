73c288801eb8bd4bccbb76ce153d5b5f4e876aae
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-07-08 19:42:20 -0400

    Added necessary comments and code cleanup

--- app/src/main/java/com/example/conversionsolution/MainActivity.kt
+++ app/src/main/java/com/example/conversionsolution/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Conversion spinner initialization
         val conversion_list = arrayOf("Select", "Weight", "Length", "Distance" )
         val conversion_spinner = findViewById<Spinner>(R.id.conversion_spinner)
         var conversion_adapter = ArrayAdapter(
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         )
         conversion_spinner.adapter = conversion_adapter
 
+        // Initialize adapter arrays for different units
         val weight_units = arrayOf("kg", "lb")
         val length_units = arrayOf("m", "cm")
         val distance_units = arrayOf("km", "mi")
@@ -58,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 
         var conversionSpinnerPosition = ""
 
+        // Conversion spinner selection listener
         conversion_spinner.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -67,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 conversionSpinnerPosition = parent?.getItemAtPosition(position).toString()
-               //Check which conversion_spinner is selected and display other spinner according to that
+                // Check which conversion_spinner is selected and display other spinner accordingly
                 when(position){
                     1 -> {
                         conversion1.adapter = weight_adapter
@@ -90,7 +93,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
+                // TODO: Handle when no conversion is selected
             }
 
 
@@ -101,6 +104,7 @@ class MainActivity : AppCompatActivity() {
         var conversionUnits1 = ""
         var conversionUnits2 = ""
 
+            // Conversion1 spinner selection listener
             conversion1.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
@@ -109,16 +113,17 @@ class MainActivity : AppCompatActivity() {
                     position: Int,
                     id: Long
                 ) {
-                    //get selected spinner value from conversion1 spinner
+                    // Get selected spinner value from conversion1 spinner
                     conversionUnits1 = parent?.getItemAtPosition(position).toString()
 
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
+                   // TODO: Handle when no conversion unit is selected for conversion1
                 }
             }
 
+                // Conversion2 spinner selection listener
                 conversion2.onItemSelectedListener = object :
                     AdapterView.OnItemSelectedListener {
                     override fun onItemSelected(
@@ -127,46 +132,44 @@ class MainActivity : AppCompatActivity() {
                         position: Int,
                         id: Long
                     ) {
-                        //get selected spinner value from conversion2 spinner
+                        // Get selected spinner value from conversion2 spinner
                         conversionUnits2 = parent?.getItemAtPosition(position).toString()
 
 
                     }
 
                     override fun onNothingSelected(parent: AdapterView<*>?) {
-                        TODO("Not yet implemented")
+                        // TODO: Handle when no conversion unit is selected for conversion2
                     }
                 }
 
         val submit = findViewById<Button>(R.id.submit)
-        //perform operations if button is clicked
+        // Perform operations when the button is clicked
         submit.setOnClickListener {
             var error = findViewById<TextView>(R.id.error)
-            //get input1 value as string
+            // Get input1 value as a string
             var inputString1 = input1.text.toString()
 
-            //If 'choose one' is selected in conversion_spinner-- code starts
-            if(conversionSpinnerPosition=="Choose One"){
+            // Check if 'Select' is selected in conversion_spinner
+            if(conversionSpinnerPosition=="Select"){
                 if(inputString1.isNotEmpty()){
-                error.setText("Please select a unit")
+                error.setText("Please select a unit.")
                 input2.setText("")
             }
                 else{
-                    error.setText("Please select a unit")
+                    error.setText("Please select a unit.")
                     input2.setText("")
                 }
             }
-            //If 'choose one' is selected in conversion_spinner-- code Ends
-
-            //If  anything is selected other than 'Choose One' in conversion_spinner-- code starts
+            // If anything is selected other than 'Select' in conversion_spinner
             else {
-                //If inputString1 is not empty--code starts
+                // Check if inputString1 is not empty
                 if (inputString1.isNotEmpty()) {
                     val inputInt1 = inputString1.toInt()
                     if (conversionUnits1 == conversionUnits2) {
                         input2.setText("$inputInt1")
                     }
-                    //conversion from km to mi or mi to km starts
+                    // Conversion from km to mi or mi to km
                     if (conversionUnits1 == "km" && conversionUnits2 == "mi") {
                         var result = inputInt1 * 0.621
                         input2.setText("$result")
@@ -175,9 +178,8 @@ class MainActivity : AppCompatActivity() {
                         var result = inputInt1 * 1.609
                         input2.setText("$result")
                     }
-                    //conversion from km to mi or mi to km Ends
 
-                    //conversion from kg to lb or lb to kg starts
+                    // Conversion from kg to lb or lb to kg
                     if (conversionUnits1 == "kg" && conversionUnits2 == "lb") {
                         var result = inputInt1 * 2.205
                         input2.setText("$result")
@@ -186,9 +188,8 @@ class MainActivity : AppCompatActivity() {
                         var result = inputInt1 * 0.453
                         input2.setText("$result")
                     }
-                    //conversion from kg to lb or lb to kg Ends
 
-                    //conversion from m to cm or cm to m starts
+                    // Conversion from m to cm or cm to m
                     if (conversionUnits1 == "m" && conversionUnits2 == "cm") {
                         var result = inputInt1 * 100.0
                         input2.setText("$result")
@@ -197,19 +198,18 @@ class MainActivity : AppCompatActivity() {
                         var result = inputInt1 * 0.01
                         input2.setText("$result")
                     }
-                    //conversion from m to cm or cm to m ends
+
                     error.setText("")
                 }
-                //If inputString1 is not empty--code ends
+                // If inputString1 is empty
 
-                //If inputString1 is empty--code starts
                 else{
-                    error.setText("enter number")
+                    error.setText("Enter a number.")
                     input2.setText("")
                 }
-                //If inputString1 is empty--code ends
+
             }
-            //If  anything is selected other than 'Choose One' in conversion_spinner-- code Ends
+
         }
     }
 
